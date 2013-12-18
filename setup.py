f854b086e671a91202ccf2ba3ce4681a70fed854
Jackie Huynh <jackie.huynh@bigcommerce.com>
2013-12-18 15:53:49 +1100

    setup.py details added

--- setup.py
+++ setup.py
@@ -6,15 +6,20 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "bigcommerce",
+    name = "Bigcommerce API Python Client",
     version = "0.9.0",
+    
+    packages=['bigcommerce'],
+    package_data = {'' : ['LICENSE', 'README.md']},
+    install_requires = ['requests>=2.0.1',
+                        'simplejson>=3.3.1'],
     author = "Bigcommerce Engineering",
     author_email = "vip@bigcommerce.com",
     description = ("Connect Python applications with the Bigcommerce API"),
     license = "MIT",
-    keywords = "example documentation tutorial",
-    url = "http://packages.python.org/an_example_pypi_project",
-    packages=['an_example_pypi_project', 'tests'],
+    keywords = "bigcommerce api client",
+    url = "https://github.com/maetl/bigcommerce-api-python/",
+    
     long_description=read('README'),
     classifiers=[
         "Development Status :: 3 - Alpha",
