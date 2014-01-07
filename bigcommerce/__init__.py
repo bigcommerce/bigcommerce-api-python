ef45d61dc09523543faf4968478d9481b29b9adb
Jackie Huynh <jackie.huynh@bigcommerce.com>
2014-01-07 11:23:06 +1100

    Removed imports for clients

--- bigcommerce/__init__.py
+++ bigcommerce/__init__.py
@@ -1,5 +1,3 @@
-from bigcommerce.api.client import Client 
 from bigcommerce.api.connection import Connection, HttpException, ClientRequestException, \
     EmptyResponseWarning, RedirectionException, ServerException
 from bigcommerce.api.oauthconnection import OAuthConnection
-from bigcommerce.api.oauthclient import OAuthClient
