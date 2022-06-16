b7a00a5d5094d37e7f94f49e7af3362729c08f97
Austin G. Smith <austin.smith@bigcommerce.com>
2022-06-16 11:25:47 -0500

    update checkout and customers v3

--- bigcommerce/resources/v3/checkouts.py
+++ bigcommerce/resources/v3/checkouts.py
@@ -1,1 +1,8 @@
-# TODO: this
+from ..base import *
+
+
+class Checkouts(UpdateableApiResource):
+    resource_version = 'v3'
+    resource_name = 'checkouts'
+
+# 9139f12e-053f-4dfc-ba8b-0e6ec4a4e4a2
--- bigcommerce/resources/v3/customers.py
+++ bigcommerce/resources/v3/customers.py
@@ -1,3 +1,21 @@
-# TODO: this
+from ..base import *
+
+
+class Customers(ListableApiResource, CreateableApiResource,
+                UpdateableApiResource, DeleteableApiResource,
+                CollectionDeleteableApiResource, CountableApiResource):
+
+    resource_name = 'customers'
+    resource_version = 'v3'
+
+class CustomerAddresses(ListableApiResource, CreateableApiResource, CollectionUpdateableApiResource, DeleteableApiResource):
+
+    resource_name = 'customers/addresses'
+    resource_version = 'v3'
+
+class CustomerFormFieldValues(ListableApiResource, UpdateableApiResource):
+
+    resource_name = 'customers/form-field-values'
+    resource_version = 'v3'
 
 # TODO: subscribers
